821af6c7b24fc5279eeb139bf2e719fd9f404662
martinMorkved <38659462+martinMorkved@users.noreply.github.com>
2020-10-09 07:43:16 +0200

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import './App.css';
 import contacts from "./contacts.js";
-import Card from "./Card.jsx";
+import Card from "./components/Card.jsx";
+import Avatar from "./components/Avatar";
 
 
 
@@ -14,22 +15,22 @@ function App() {
       
     <div>
       <h1 className="heading">My Contacts</h1>
-
+      <Avatar img="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTGbWN0KpNPejhkQTGODE5zp7_P03OvFNaf8g&usqp=CAU" />
       <Card 
         name={contacts[0].name}
-        imgURL ={contacts[0].imgURL}
+        img ={contacts[0].imgURL}
         phone= {contacts[0].phone}
         email={contacts[0].email}
         />
         <Card 
         name={contacts[1].name}
-        imgURL ={contacts[1].imgURL}
+        img ={contacts[1].imgURL}
         phone= {contacts[1].phone}
         email={contacts[1].email}
         />
         <Card 
         name={contacts[2].name}
-        imgURL ={contacts[2].imgURL}
+        img ={contacts[2].imgURL}
         phone= {contacts[2].phone}
         email={contacts[2].email}
         />
---
+++ src/components/Card.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import Avatar from "./Avatar.jsx";
+
+function Card(props) {
+    return (
+      
+      <div>
+        
+        <div className="card">
+          <div className="top">
+            <h2 className="name">{props.name}</h2>
+            <Avatar img={props.img} />
+          </div>
+          <div className="bottom">
+            <p className="info">{props.phone}</p>
+            <p className="info">{props.email}</p>
+          </div>
+        </div>
+      </div>
+    )
+  };
+
+  export default Card;
